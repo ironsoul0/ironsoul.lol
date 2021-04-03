3c453cf99b89c47fa7b3bb83ae417a025722ea5b
ironsoul0 <timka2609@gmail.com>
2021-04-03 23:35:40 +0600

    feat: configure tailwind and sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
-import Link from "next/link";
+import React from "react";
 
 const IndexPage = () => (
-  <>
-    <h1>Hello Next.js</h1>
-    <p>kek</p>
-    <p>kek</p>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </>
+  <div className="max-w-md mx-auto text-center py-4">
+    <h1 className="font-bold">Hello Next.js</h1>
+    <p>Most opinionated Next.js setup</p>
+  </div>
 );
 
 export default IndexPage;
