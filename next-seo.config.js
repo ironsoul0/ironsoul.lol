68fae1089e78f613cc9e5181072b6bd57db6ac68
ironsoul0 <timka2609@gmail.com>
2021-04-08 22:43:00 +0600

    feat: add next-seo for SEO optimization

---
+++ next-seo.config.js
@@ -0,0 +1,31 @@
+export default {
+  title: "ironsoul | Temirzhan Yussupov",
+  description: "Real full-stack ninja lives here.",
+  canonical: "https://ironsoul.ninja",
+  openGraph: {
+    url: "https://ironsoul.ninja",
+    title: "ironsoul | Temirzhan Yussupov",
+    description: "Real full-stack ninja lives here.",
+    images: [
+      {
+        url: "https://ironsoul.ninja/og-image-01.png",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+      },
+      {
+        url: "https://ironsoul.ninja/og-image-02.png",
+        width: 800,
+        height: 800,
+        alt: "Og Image Alt 2",
+      },
+      {
+        url: "https://ironsoul.ninja/og-image-03.png",
+        width: 900,
+        height: 800,
+        alt: "Og Image Alt 3",
+      },
+    ],
+    site_name: "ironsoul | Temirzhan Yussupov",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import "styles/index.scss";
 
 import { Layout } from "components";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
+
+import SEO from "../next-seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <DefaultSeo {...SEO} />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
